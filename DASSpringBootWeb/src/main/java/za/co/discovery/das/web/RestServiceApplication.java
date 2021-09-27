213c8c88c01b84b1fd6a42ac12e0e01b29f2f6d9
LizaSteyn <71076486+LizaSteyn@users.noreply.github.com>
2021-09-27 14:17:39 +0200

    SpringBoot

---
+++ DASSpringBootWeb/src/main/java/za/co/discovery/das/web/RestServiceApplication.java
@@ -0,0 +1,11 @@
+package za.co.discovery.das.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
---
+++ DASSpringBootWeb/src/main/java/za/co/discovery/das/web/controller/DemoController.java
@@ -0,0 +1,5 @@
+package za.co.discovery.das.web.controller;
+
+
+public class DemoController {
+}
